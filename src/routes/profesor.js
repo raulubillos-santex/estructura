b0ef6874c86daee7eee54b089945a277f49bae74
Hernan Mauricio Pena <esingcomputacion@gmail.com>
2022-07-25 19:49:52 -0300

    Fix varios

--- src/routes/profesor.js
+++ src/routes/profesor.js
@@ -1,15 +1,11 @@
 var express = require('express');
-const {deleteProfesorByIdProfesor, getProfesorByIdProfesor, patchProfesorByIdProfesor, postProfesor, putProfesorByIdProfesor} = require('../controllers/profesor');
+const {postProfesor, getProfesorByIdProfesor, putProfesor} = require('../controllers/profesor');
 var router = express.Router();
 
 router.post('/', postProfesor);
 
 router.get('/:idProfesor', getProfesorByIdProfesor);
 
-router.put('/:idProfesor', putProfesorByIdProfesor);
-
-router.patch('/:idProfesor', patchProfesorByIdProfesor);
-
-router.delete('/:idProfesor', deleteProfesorByIdProfesor);
+router.put('/:idProfesor', putProfesor);
 
 module.exports = router;
